52f92a41caca7b2c2aef49f13f3995907760059f
Pontus Stjernström <pontus@stjernstrom.nu>
2021-08-27 08:09:51 +0200

    Manually add first track

--- src/Point.h
+++ src/Point.h
@@ -6,11 +6,18 @@
 struct Point
 {
   float x, y;
+
   Point() {}
   Point(float x, float y) : x(x), y(y) {}
+
   b2Vec2 to_b2Vec2()
   {
     return b2Vec2(x, y);
+  }
+
+  Point add(float x, float y)
+  {
+    return Point(this->x + x, this->y + y);
   }
 };
 
--- src/Track.cpp
+++ src/Track.cpp
@@ -1,5 +1,7 @@
 #include "track.h"
 
+#include <math.h>
+
 Track::Track(string name, b2World *phys_world) : width(40), height(30)
 {
   segments = create_track(name, phys_world);
@@ -14,6 +16,8 @@ vector<TrackSegment> Track::get_segments()
 
 vector<TrackSegment> Track::create_track(string name, b2World *phys_world)
 {
+  float track_width = 6;
+
   if (name == "default")
   {
     vector<Point> outer_points = {
@@ -26,7 +30,43 @@ vector<TrackSegment> Track::create_track(string name, b2World *phys_world)
         Point(width - 1, (height / 2) - 1),
         Point(width - 1, 1),
         Point(1, 1)};
-    TrackSegment segment = TrackSegment(outer_points, phys_world);
-    return vector<TrackSegment>{segment};
+
+    vector<Point> inner_points = {
+        outer_points[8].add(track_width, track_width),
+        outer_points[7].add(-track_width, track_width),
+        outer_points[6].add(-track_width, -track_width),
+        outer_points[5].add(-track_width, -track_width),
+        outer_points[4].add(-track_width, track_width),
+        outer_points[3].add(-track_width, track_width),
+        outer_points[2].add(-track_width, -track_width),
+        outer_points[1].add(track_width, -track_width),
+        outer_points[0].add(track_width, track_width)};
+
+    return vector<TrackSegment>{
+        TrackSegment(outer_points, phys_world),
+        TrackSegment(inner_points, phys_world)};
   }
+}
+
+// This is a test for the future when auto-generating tracks
+vector<Point> create_inner_points(vector<Point> outer_points)
+{
+  float width = 10;
+
+  vector<Point> inner_points;
+  for (int i = 1; i < outer_points.size() - 1; i++)
+  {
+    Point prev_outer = outer_points[i - 1];
+    Point outer = outer_points[i];
+    Point next_outer = outer_points[i + 1];
+
+    float outer_heading = atan2(next_outer.x - outer.x, next_outer.y - outer.y);
+    float prev_heading = atan2(outer.x - prev_outer.x, outer.y - prev_outer.y);
+
+    inner_points.push_back(Point(
+        outer.x + cos(outer_heading) * width + cos(prev_heading) * width,
+        outer.y - sin(outer_heading) * width - sin(prev_heading) * width));
+  }
+
+  return inner_points;
 }
